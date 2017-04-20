6db20af97bba7d2b3ae5b3a43e84e6ec683c5b85
maym2104 <mariane.maynard@usherbrooke.ca>
2017-04-20 09:16:30 -0400

    Files from Sherjil Ozair

---
+++ dataprep.py
@@ -0,0 +1,17 @@
+import imageio
+import glob
+import numpy as np
+from tqdm import tqdm
+
+home = 'C:\\Users\\Mariane\\Documents\\ift6266\\ift6266-h17-project\\'
+path = 'coco\\images\\val2014'
+
+images = []
+
+for fname in tqdm(glob.glob('{}\\*.jpg'.format(home+path))):
+    img = imageio.imread(fname)
+    if img.shape == (64, 64, 3) and img.dtype == np.uint8:
+        images.append(img)
+
+images = np.array(images)
+np.savez_compressed(home+'images.valid.npz', images)
